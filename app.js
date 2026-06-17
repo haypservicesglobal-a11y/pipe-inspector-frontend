a73ca6f9124066b23a04bf2ae1c499aafb862ac1
haypservicesglobal-a11y <haypservicesglobal@gmail.com>
2026-06-17 06:57:40 +0100

    Update API connection endpoint route to production cloud backend

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", () => {
         formData.append("file", activeFile);
 
         try {
-            const response = await fetch("http://127.0.0", {
+            const response = await fetch("https://pipe-inspector-backend-production.up.railway.app", {
                 method: "POST",
                 body: formData
             });
